c4aea3b395456cd5a608ec85b4de667f85815069
Andre Lino <andre.lino@trigniconica.com>
2024-12-11 17:13:18 +0000

    Added: defineProps and ref

---
+++ src/components/GenericComp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import { ref, Ref } from 'vue'
+    const { msg = 'hello'} = defineProps<{ msg?: string }>()
+    const year: Ref<string | number> = ref(2020)
+</script>
+
+<template>
+    <div class="box">
+        <h1>TypeScript Fun</h1>
+        <div>Parent Sent: {{ msg }}</div>
+        {{ year }}
+    </div>
+</template>
+
+<style scoped>
+/* Your component styles go here */
+</style>
